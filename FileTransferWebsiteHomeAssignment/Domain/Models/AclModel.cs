320d04120ae134fa5e6d6be172812669cc431db5
NicoleAquilina <nicole.aquilina.e21870@mcast.edu.mt>
2022-12-13 10:40:13 +0100

    Project References and Website Creation

--- FileTransferWebsiteHomeAssignment/Domain/Models/AclModel.cs
+++ FileTransferWebsiteHomeAssignment/Domain/Models/AclModel.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Domain.Models
 {
     public class AclModel
     {
+        [Key]
+        public int Id { get; set; }
+        public string Username { get; set; }
+
+       // [ForeignKey("TextFileModel")] still neded to check
+        public Guid FileName { get; set; }
     }
 }
--- FileTransferWebsiteHomeAssignment/Domain/Models/TextFileModel.cs
+++ FileTransferWebsiteHomeAssignment/Domain/Models/TextFileModel.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Domain.Models
 {
     public class TextFileModel
-    {
+    { 
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string Data { get; set; }
+        public Guid FileName { get; set; }
+        public DateTime UploadedOn { get; set;}
+        public string Author { get; set; }
+        public DateTime LastUpdated { get; set; }
+
     }
 }
